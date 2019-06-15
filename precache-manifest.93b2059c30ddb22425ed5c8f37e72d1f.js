0330c1b2efd1a49081f8d38240a8245c8a4db2ed
Malki Davis <md01@me.com>
2019-06-16 00:40:06 +0300

    Updates

---
+++ precache-manifest.93b2059c30ddb22425ed5c8f37e72d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473bf83a42c01ddec4f0a6d9bc750cc6",
+    "url": "/pathgenerator/index.html"
+  },
+  {
+    "revision": "082a472f09a47c01f3f5",
+    "url": "/pathgenerator/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pathgenerator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "082a472f09a47c01f3f5",
+    "url": "/pathgenerator/static/js/main.db166964.chunk.js"
+  },
+  {
+    "revision": "7592e9f63b262e833632",
+    "url": "/pathgenerator/static/js/runtime~main.0e094d83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pathgenerator/static/media/logo.5d5d9eef.svg"
+  }
+]);
